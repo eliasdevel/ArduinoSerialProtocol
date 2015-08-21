59c149f8796cf04ebefdd62e63255a2c8e9409d0
Administrator <elias.muller1@gmail.com>
2015-08-20 22:57:23 -0300

    first

---
+++ SerialprotocolArduino.ino
@@ -0,0 +1,62 @@
+//command serial protocol standarts [command:value]
+String analizer;
+boolean reading = false;
+
+void setup()
+{
+  //set the mode for the ports
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  protocol();
+}
+
+//analit Serial for start an end listening protocol
+void protocol() {
+  char c = Serial.read();
+
+  if ( c == '[' ) {
+    reading = true;
+  }
+
+  if (reading && c != -1) {
+    analizer += c;
+  }
+
+  if (  c == ']' ) {
+    reading = false;
+    parser(analizer);
+    analizer = "";
+  }
+
+}
+
+void parser(String analiser) {
+  analiser.replace("[", "");
+  analiser.replace("]", "");
+  int indexSeparator = analiser.indexOf(':');
+  int lastIndex = analiser.length();
+
+  String command = analiser.substring(0, indexSeparator);
+  String value = analiser.substring(indexSeparator + 1, lastIndex);
+
+  caller(command, value);
+}
+
+void caller(String command, String value) {
+  if (command == "on") putOn(value.toInt());
+  if (command == "off") putOff(value.toInt());
+}
+
+void putOn(int port) {
+  digitalWrite(port, HIGH);
+}
+void putOff(int port) {
+  digitalWrite(port, LOW);
+}
